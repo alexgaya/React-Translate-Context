f82eb780f37c371bbe76817977a9dd2accc8d926
m0rem0ney <alejandrogaya1994@gmail.com>
2019-06-19 13:15:38 +0200

    add store

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,20 @@
 import React from 'react'
 import UserCreate from './UserCreate'
-import LanguageContext from '../contexts/LanguageContext'
+import { LanguageStore } from '../contexts/LanguageContext'
 import ColorContext from '../contexts/ColorContext'
+import LanguageSelector from './LanguageSelector'
 
 class App extends React.Component {
-
-    state = {
-        language: 'english',
-        color: 'red'
-    }
-
-    onLanguageChange = language => {
-        this.setState({ language })
-    }
 
     render() {
         return (
             <div className="ui container">
-                <div>
-                    Select a language:
-                    <i className="flag us" onClick={() => this.onLanguageChange('english')}></i>
-                    <i className="flag nl" onClick={() => this.onLanguageChange('dutch')}></i>
-                </div>
-                <LanguageContext.Provider value={this.state.language}>
-                    <ColorContext.Provider value={this.state.color}>
+                <LanguageStore>
+                    <LanguageSelector />
+                    <ColorContext.Provider value='red'>
                         <UserCreate />
                     </ColorContext.Provider>
-                </LanguageContext.Provider>
+                </LanguageStore>
             </div>
         )
     }
